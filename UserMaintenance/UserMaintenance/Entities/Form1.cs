cb3e884f0e80a6c9bd3488ce66ce66588ce5d0ad
Patrik Pilinyi <patrik.pilinyi@stud.uni-corvinus.hu>
2020-09-27 23:07:18 +0200

    q

---
+++ UserMaintenance/UserMaintenance/Entities/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserMaintenance.Entities
+{
+    public partial class Form1 : Form
+    {
+        BindingList<User_cs> users = new BindingList<User_cs>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = Resource1.LastName; // label1
+            label2.Text = Resource1.FirstName; // label2
+            button1.Text = Resource1.Add; // button1
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+            var u = new User_cs()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text,
+            };
+            users.Add(u);
+        }
+
+
+    }
+}
